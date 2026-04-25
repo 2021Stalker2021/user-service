c0988d4ddaab4ffd9ef93b0dc6570e4724c66f6d
2021Stalker2021 <edik.korben@gmail.com>
2026-04-25 18:05:27 +0300

    feat: integrate Swagger UI and HATEOAS support

--- src/main/java/io/gsc/controller/UserController.java
+++ src/main/java/io/gsc/controller/UserController.java
@@ -6,6 +6,8 @@ import io.gsc.model.request.NewUserRequest;
 import io.gsc.model.request.UpdateUserRequest;
 import io.gsc.service.UserService;
 import io.gsc.utils.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -14,30 +16,44 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 @RestController
 @Validated
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Tag(name = "Пользователи", description = "API для управления данными пользователей")
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
+    @Operation(summary = "Получить пользователя по ID", description = "Возвращает данные пользователя и ссылки на возможные действия")
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
         log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
         UserDTO response = userService.getById(id);
+
+        response.add(linkTo(methodOn(UserController.class).getUserById(id)).withSelfRel());
+        response.add(linkTo(methodOn(UserController.class).deleteUser(id)).withRel("delete"));
+        response.add(linkTo(methodOn(UserController.class).updateUserById(id, null)).withRel("update"));
+
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Создать нового пользователя")
     @PostMapping("/create")
     public ResponseEntity<UserDTO> createUser(@Validated @RequestBody NewUserRequest request) {
         log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
         UserDTO response = userService.createUser(request);
+
+        response.add(linkTo(methodOn(UserController.class).getUserById(response.getId())).withSelfRel());
+
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Обновить данные пользователя")
     @PutMapping("/update/{id}")
     public ResponseEntity<UserDTO> updateUserById(
             @PathVariable(name = "id") Long userId,
@@ -45,13 +61,14 @@ public class UserController {
         log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
         UserDTO updatedUser = userService.updateUser(userId, request);
+        updatedUser.add(linkTo(methodOn(UserController.class).getUserById(userId)).withSelfRel());
+
         return ResponseEntity.ok(updatedUser);
     }
 
+    @Operation(summary = "Удалить пользователя")
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteUser(
-            @PathVariable(name = "id") Long userId
-    ) {
+    public ResponseEntity<Void> deleteUser(@PathVariable(name = "id") Long userId) {
         log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
         userService.delete(userId);
--- src/main/java/io/gsc/model/dto/UserDTO.java
+++ src/main/java/io/gsc/model/dto/UserDTO.java
@@ -3,13 +3,14 @@ package io.gsc.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
+public class UserDTO extends RepresentationModel<UserDTO> {
     private Long id;
 
     private String name;
